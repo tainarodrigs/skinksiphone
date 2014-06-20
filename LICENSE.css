453f850165c84bef8594c626068c378e3b7d88e2
tainarodrigs <taina-q@hotmail.com>
2014-06-20 05:39:00 -0300

    Create LICENSE.css

---
+++ LICENSE.css
@@ -0,0 +1,30 @@
+#player,#player a{ color:#696969; }
+#player{ background:url http://static.tumblr.com/g0p2wwu/c7Qmufkl2/preta.png; background-position:bottom; }
+#player .mainImage{ background:url (http://static.tumblr.com/tqw05bg/FrFn7gjmt/skin_2_-_copia.png) ; }
+ 
+#player #baseLeft{ width:93px; }
+#player #baseRight{ width:145px;}
+#player .button{  width:26px; }
+#player #play{ background:url (http://static.tumblr.com/tqw05bg/PKCn7gjof/iphone44.png); }
+#player #play,#player #pause{ left:26px; }
+#player #previous{ left:0px; }
+#player #next{ left:52px; }
+#player #title{ left:90px;  }
+#player #tooltip{ right:196px; }
+#player #timer{ right:120px;  }
+#player #list{ right:0px; }
+ 
+#player #seeker{ right:320px; }
+#player #seekerBase{ border-color:#fff;   }
+#player #seekerBuffered{ background-color:#4b575d; }
+#player #seekerSliderBase{ background-color:#ccc; }
+ 
+#player #volume{ width:65px; right:39px; }
+#player #volumeSlider{ width:8px; background:url (http://static.tumblr.com/g0p2wwu/tpqmufkju/branca.png); }
+ 
+#playlist,#playlist .module,#playlist h3{ border-color:#fff;}
+#playlist .module{  color:#333; background-color:#fff; }
+#playlist h3{ background:url(); color:#000; background-color:#fff;  }
+#playlist .even{background-color:#fff;}
+#playlist .odd{ background-color:#fff;}
+#playlist .focus{ background-color:#fff; color:#999;}
